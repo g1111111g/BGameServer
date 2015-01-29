66690e5a14df551df0e08b10fa93e25ccca5bc82
root <root@AY130710003303Z.(none)>
2015-01-29 17:31:23 +0800

    protobuf service

--- classes/Base.php
+++ classes/Base.php
@@ -1,5 +1,13 @@
 <?php
+require_once('protocol/pb_proto_foo.php');
 class Base{
-	
+	function test($a, $b){
+		$calc = $a + $b;
+		$result = new Result();
+		$result->setA($a);
+		$result->setB($b);
+		$result->setC($calc);
+		return $result;
+	}	
 }
 ?>
--- service.php
+++ service.php
@@ -1,3 +1,39 @@
 <?php
-fastcgi_finish_request();
+require_once('exception/GameException.php');
+foreach(glob('classes/*.php') as $class){
+	include_once $class;
+}
+//fastcgi_finish_request();
+$className = $_GET['c'];
+$methodName = $_GET['f'];
+$useBin = $_GET['b'];
+$methodParams = json_decode($_GET['p'], true);
+try{
+	if(isset($_POST['req'])){
+		$reqInfo = new RemoteCall();
+		$reqInfo->parseFromString($_POST['req']);
+		$className = $reqInfo->getClassName();
+		$methodName = $reqInfo->getFunctionName();
+		$methodParams = array();
+		for($iterator = $reqInfo->getParamsIterator(); $iterator->valid(); $iterator->next()){
+			$key = $iterator->current()->getParamKey();
+			$value = $iterator->current()->getParamValue();
+			$methodParams[$key] = $value;
+		}
+	}
+	$organizedparams = array();
+	$classObj = new ReflectionClass($className);
+	$methodObj = $classObj->getMethod($methodName);
+	$instance = $classObj->newInstanceArgs();
+	$invokeResult = $methodObj->invokeArgs($instance, $methodParams);
+	echo $invokeResult->serializeToString();
+}catch(Exception $e){
+	if($e instanceof GameException){
+		$gameException = $e;	
+	}else{
+		$gameException = new GameException($e->getMessage(), $e->getCode());
+	}
+	//echo $gameException->toBinData($useBin);
+	echo $e->getMessage();
+}
 ?>
